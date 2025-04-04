568299f52173cc67ae514187bd817bdc77abcaa4
Jeongwon Park <pjw200116@gmail.com>
2025-04-05 00:07:47 +0900

    250405commit

--- Baekjoon_python/BJ_11725.py
+++ Baekjoon_python/BJ_11725.py
@@ -1,4 +1,5 @@
 import sys
+sys.setrecursionlimit(10**6)
 
 input= sys.stdin.readline
 
@@ -16,7 +17,16 @@ for _ in range(N-1):
     tree[a].append(b)
     tree[b].append(a)
 
+parent = [0] * (N+1)
 
+def dfs(node):
+    for child in tree[node]:
+        if parent[child] == 0:
+            parent[child] = node
+            dfs(child)
 
+parent[1] = 1
+dfs(1)
 
-
+for i in range(2, N+1):
+    print(parent[i])
